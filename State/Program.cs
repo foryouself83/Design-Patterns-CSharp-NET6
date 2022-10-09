837ef5abf06a7d793444f1d69b39f54e5056edcb
bsyoo <foryouself83@gmail.com>
2022-10-09 22:58:46 +0900

    fix: Add mark

--- State/Program.cs
+++ State/Program.cs
@@ -4,15 +4,20 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        // Change Guest User
         var context = new UserContext();
-
         context.Report();
+
+        // Change FMEA User
         context.SetUserType(State.Enum.UserType.Fmea);
         context.Report();
+        // Change FTA User
         context.SetUserType(State.Enum.UserType.Fta);
         context.Report();
+        // Change STPA User
         context.SetUserType(State.Enum.UserType.Stpa);
         context.Report();
+        // Change Ultimate User
         context.SetUserType(State.Enum.UserType.Ultimate);
         context.Report();
     }
